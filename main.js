66126f8262d6096768ab9e2f56c8133ef2e08b91
dkzerfu <dkzerfu@gmail.com>
2021-01-28 18:49:08 -0500

    second edit

--- main.js
+++ main.js
@@ -1,19 +1,12 @@
 console.log("connected!");
 
-
-
-
-var display = document.querySelector(".twood");
-var inputText = document.querySelector("#twood-input");
 var button = document.querySelector("#submit-twood");
+
 button.addEventListener("click", function() {
-
-
+  
+  var display = document.querySelector(".twood");
+  var inputText = document.querySelector("#twood-input");
   display.innerText = inputText.value;
-
-
-
-
 
 })
 
